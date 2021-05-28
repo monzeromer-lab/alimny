bb661e25c24f01446f987e4aa3eebd1ac60bb72e
Monzer Abdullaziz <nezonru87@yahoo.com>
2021-05-28 10:24:42 +0200

    imported student and teachers update endpoint to there index files

--- api/users/student/index.js
+++ api/users/student/index.js
@@ -4,4 +4,6 @@ index.use("/students/login" , require("./login"));
 
 index.use("/students/signup" , require("./signup"));
 
+index.use("/students/update" , require("./update"));
+
 module.exports = index;
--- api/users/teacher/index.js
+++ api/users/teacher/index.js
@@ -4,4 +4,6 @@ index.use("/teachers/login" , require("./login"));
 
 index.use("/teachers/signup" , require("./signup"));
 
+index.use("/teachers/update" , require("./update"));
+
 module.exports = index;
